bc63c03fa3ecc3bd260470a3adf804b52f32bd47
VanVuk4 <142598392+noobCode24@users.noreply.github.com>
2024-06-24 23:12:08 +0700

    checkout.php

--- app/Views/clients/Book_tickets/checkout.php
+++ app/Views/clients/Book_tickets/checkout.php
@@ -9,22 +9,27 @@
             </div>
             <div class="social-media">
                 <a href="https://www.facebook.com/congvienthienduongbaoson/">
-                    <img src="https://th.bing.com/th/id/R.2bad70f2d08429a28dfbebd4c237924b?rik=vgEdhJ%2f%2biiEnQQ&riu=http%3a%2f%2fpngimg.com%2fuploads%2ffacebook_logos%2ffacebook_logos_PNG19748.png&ehk=0ZiZ04ZZ6mSJ5oyPxBh1gy4FSYhegWTWyDpCiI73sbw%3d&risl=&pid=ImgRaw&r=0" alt="Facebook" style="width: 30px;">
+                    <img src="https://th.bing.com/th/id/R.2bad70f2d08429a28dfbebd4c237924b?rik=vgEdhJ%2f%2biiEnQQ&riu=http%3a%2f%2fpngimg.com%2fuploads%2ffacebook_logos%2ffacebook_logos_PNG19748.png&ehk=0ZiZ04ZZ6mSJ5oyPxBh1gy4FSYhegWTWyDpCiI73sbw%3d&risl=&pid=ImgRaw&r=0"
+                        alt="Facebook" style="width: 30px;">
                 </a>
                 <a href="https://www.youtube.com/channel/UC6ZAzgoSBsqPvYaGOhfZ8Lw">
-                    <img src="https://th.bing.com/th/id/OIP.OVUMFVp8elGfMYh-27fTLAHaFO?rs=1&pid=ImgDetMain" alt="YouTube" style="width: 30px;">
+                    <img src="https://th.bing.com/th/id/OIP.OVUMFVp8elGfMYh-27fTLAHaFO?rs=1&pid=ImgDetMain"
+                        alt="YouTube" style="width: 30px;">
                 </a>
                 <a href="https://www.instagram.com/thienduong.baoson/?hl=en">
-                    <img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-social-platform-icon-png-image_6315976.png" alt="Instagram" style="width: 25px;"></a>
+                    <img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-social-platform-icon-png-image_6315976.png"
+                        alt="Instagram" style="width: 25px;"></a>
             </div>
             <a class="buy-ticket"><i class="fa-solid fa-cart-shopping"></i>
                 <p>MUA VÉ</p> <span id="quantity_checkout_ticket">0</span>
             </a>
             <div class="user-menu">
-                <img src="https://th.bing.com/th/id/OIP.N_YBvMrwwjlxzOtw-UoHawAAAA?w=435&h=435&rs=1&pid=ImgDetMain" alt="User" style="width: 33px; margin-right: 5px;">
+                <img src="https://th.bing.com/th/id/OIP.N_YBvMrwwjlxzOtw-UoHawAAAA?w=435&h=435&rs=1&pid=ImgDetMain"
+                    alt="User" style="width: 33px; margin-right: 5px;">
             </div>
             <div class="language">
-                <img src="https://media.istockphoto.com/vectors/vietnamese-flag-vector-id864417828?k=6&m=864417828&s=612x612&w=0&h=AbGtQWE0vfKupO0Tpp8ga49MVZq4O2P7HIkIOUxl2rk=" alt="Vietnamese" style="width: 34px; margin-right: 5px;">
+                <img src="https://media.istockphoto.com/vectors/vietnamese-flag-vector-id864417828?k=6&m=864417828&s=612x612&w=0&h=AbGtQWE0vfKupO0Tpp8ga49MVZq4O2P7HIkIOUxl2rk="
+                    alt="Vietnamese" style="width: 34px; margin-right: 5px;">
                 <i class="fa-solid fa-caret-down"></i>
             </div>
         </div>
@@ -43,7 +48,7 @@
                 </a>
             </div>
             <div class="progress-step" id="step2">
-                <a href="./ticket-list.html">
+                <a href="<?php echo _HOST_PATH_ ?>/clients/Tickets">
                     <div class="step-label">Chọn vé</div>
                     <i class="fas fa-ticket-alt step-icon"></i>
                 </a>
@@ -131,13 +136,13 @@
                             <div class="payment-method_checkout">
                                 <div class="form-visa">
                                     <input type="radio" id="visa" name="payment" value="visa" checked>
-                                    <img src="<?php echo _HOST_PATH_ ?>\public\assets\clients\images\book_tickets_img\visa.png" alt="">
+                                    <img src="assets/img/visa.png" alt="">
                                     <label for="visa">Thẻ quốc tế</label>
                                 </div>
                                 <div class="line2_checkout"></div>
                                 <div class="form-domestic">
                                     <input type="radio" id="domestic" name="payment" value="domestic">
-                                    <img src="<?php echo _HOST_PATH_ ?>\public\assets\clients\images\book_tickets_img\atm.png" alt="">
+                                    <img src="assets/img/atm.png" alt="">
                                     <label for="domestic">Thẻ/Tài khoản ngân hàng nội địa</label>
                                 </div>
                                 <div class="line2"></div>
@@ -188,8 +193,8 @@
                         </div>
                     </div>
                     <div class="order-actions">
-                        <button class="back-button">Quay lại</button>
-                        <button type="submit" class="checkout-button">THANH TOÁN</button>
+                        <button class="back-button"></button>
+                        <button type="submit" class="checkout-button" id="confirm-btn-checkout">THANH TOÁN</button>
                     </div>
                 </div>
             </div>
