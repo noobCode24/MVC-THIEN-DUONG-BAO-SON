fd35422e93a1c62f550efae6ba4650ebed9a5b74
VanVuk4 <142598392+noobCode24@users.noreply.github.com>
2024-06-28 17:21:59 +0700

    Update ticket-list.php

--- app/Views/clients/Book_tickets/ticket-list.php
+++ app/Views/clients/Book_tickets/ticket-list.php
@@ -9,22 +9,27 @@
             </div>
             <div class="social-media">
                 <a href="https://www.facebook.com/congvienthienduongbaoson/">
-                    <img src="https://th.bing.com/th/id/R.2bad70f2d08429a28dfbebd4c237924b?rik=vgEdhJ%2f%2biiEnQQ&riu=http%3a%2f%2fpngimg.com%2fuploads%2ffacebook_logos%2ffacebook_logos_PNG19748.png&ehk=0ZiZ04ZZ6mSJ5oyPxBh1gy4FSYhegWTWyDpCiI73sbw%3d&risl=&pid=ImgRaw&r=0" alt="Facebook" style="width: 30px;">
+                    <img src="https://th.bing.com/th/id/R.2bad70f2d08429a28dfbebd4c237924b?rik=vgEdhJ%2f%2biiEnQQ&riu=http%3a%2f%2fpngimg.com%2fuploads%2ffacebook_logos%2ffacebook_logos_PNG19748.png&ehk=0ZiZ04ZZ6mSJ5oyPxBh1gy4FSYhegWTWyDpCiI73sbw%3d&risl=&pid=ImgRaw&r=0"
+                        alt="Facebook" style="width: 30px;">
                 </a>
                 <a href="https://www.youtube.com/channel/UC6ZAzgoSBsqPvYaGOhfZ8Lw">
-                    <img src="https://th.bing.com/th/id/OIP.OVUMFVp8elGfMYh-27fTLAHaFO?rs=1&pid=ImgDetMain" alt="YouTube" style="width: 30px;">
+                    <img src="https://th.bing.com/th/id/OIP.OVUMFVp8elGfMYh-27fTLAHaFO?rs=1&pid=ImgDetMain"
+                        alt="YouTube" style="width: 30px;">
                 </a>
                 <a href="https://www.instagram.com/thienduong.baoson/?hl=en">
-                    <img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-social-platform-icon-png-image_6315976.png" alt="Instagram" style="width: 25px;"></a>
+                    <img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-social-platform-icon-png-image_6315976.png"
+                        alt="Instagram" style="width: 25px;"></a>
             </div>
             <a class="buy-ticket"><i class="fa-solid fa-cart-shopping"></i>
                 <p>MUA VÉ</p> <span>0</span>
             </a>
             <div class="user-menu">
-                <img src="https://th.bing.com/th/id/OIP.N_YBvMrwwjlxzOtw-UoHawAAAA?w=435&h=435&rs=1&pid=ImgDetMain" alt="User" style="width: 33px; margin-right: 5px;">
+                <img src="https://th.bing.com/th/id/OIP.N_YBvMrwwjlxzOtw-UoHawAAAA?w=435&h=435&rs=1&pid=ImgDetMain"
+                    alt="User" style="width: 33px; margin-right: 5px;">
             </div>
             <div class="language">
-                <img src="https://media.istockphoto.com/vectors/vietnamese-flag-vector-id864417828?k=6&m=864417828&s=612x612&w=0&h=AbGtQWE0vfKupO0Tpp8ga49MVZq4O2P7HIkIOUxl2rk=" alt="Vietnamese" style="width: 34px; margin-right: 5px;">
+                <img src="https://media.istockphoto.com/vectors/vietnamese-flag-vector-id864417828?k=6&m=864417828&s=612x612&w=0&h=AbGtQWE0vfKupO0Tpp8ga49MVZq4O2P7HIkIOUxl2rk="
+                    alt="Vietnamese" style="width: 34px; margin-right: 5px;">
                 <i class="fa-solid fa-caret-down"></i>
             </div>
         </div>
@@ -65,7 +70,8 @@
 
     <!-- end orientation -->
 
-    <div class="banner_ticket">
+    <div class="banner_ticket banner"
+        style="background-image: url('<?php echo _HOST_PATH_ ?>/public/assets/clients/images/book_tickets_img/bg-page.png');">
         <div class="top-bar">
             <div class="filter-selection-ticket">
                 <div class="filter-btn active">
@@ -94,36 +100,44 @@
         <!-- end top-bar -->
 
         <div class="ticket-selection">
-            <div class="ticket">
-                <img src="<?php echo _HOST_PATH_ ?>\public\assets\clients\images\book_tickets_img\ticket_1-3.png" alt="Ticket 1">
-                <span id="ticketId" style="display: none;">1</span>
-
-                <div class="ticket-details">
-                    <div class="content-ticket">
-                        <p><i class="fas fa-ticket-alt"></i>Vé tham quan khách cao từ 1m - 1,3m</p>
-                    </div>
-                    <div class="sub-content-ticket-details">
-                        <span>Chi tiết <i class="fa-solid fa-angle-right"></i></span>
-                    </div>
-                    <hr class="hr_ticket_line">
-                    <hr class="hr_ticket_line">
-                    <div class="ticket-price">
-                        <span>320,000 VND</span>
-                        <div class="ticket-controls">
-                            <button class="decrease" data-price="320000"><i class="fa-solid fa-minus"></i></button>
-                            <span class="ticket-quantity">0</span>
-                            <button class="increase" data-price="320000"><i class="fa-solid fa-plus"></i></button>
+            <?php
+            if (!empty($ticketList)) {
+                foreach ($ticketList as $key => $value) {
+                    ?>
+                    <div class="ticket">
+                        <img src="<?php echo _HOST_PATH_ ?>\public\assets\admin\images\ticket_type_image\<?php echo $value['ticket_icon'] ?>"
+                            alt="Ticket <?php echo $value['ticket_id'] ?>">
+                        <span id="ticketId" style="display: none;"><?php echo $value['ticket_id'] ?></span>
+                        <div class="ticket-details">
+                            <div class="content-ticket">
+                                <p><i class="fas fa-ticket-alt"></i><?php echo $value['ticket_name'] ?></p>
+                            </div>
+                            <div class="sub-content-ticket-details">
+                                <span>Chi tiết <i class="fa-solid fa-angle-right"></i></span>
+                            </div>
+                            <hr class="hr_ticket_line">
+                            <hr class="hr_ticket_line">
+                            <div class="ticket-price">
+                                <span><?php echo $value['price'] ?> VND</span>
+                                <div class="ticket-controls">
+                                    <button class="decrease" data-price="320000"><i class="fa-solid fa-minus"></i></button>
+                                    <span class="ticket-quantity">0</span>
+                                    <button class="increase" data-price="320000"><i class="fa-solid fa-plus"></i></button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                    <?php
+                }
+            }
+            ?>
 
             <!-- <div class="img-ticket">
                     <div class="img-ticket-container">
                         <img src="<?php echo _HOST_PATH_ ?>\public\assets\clients\images\book_tickets_img\ticket_1-3.pngticket_1-3.png" alt="Ticket 1">
                     </div>
                 </div> -->
-            <div class="ticket">
+            <!-- <div class="ticket">
                 <img src="<?php echo _HOST_PATH_ ?>\public\assets\clients\images\book_tickets_img\ticket_up_13.png" alt="Ticket 2">
                 <span id="ticketId" style="display: none;">2</span>
                 <div class="ticket-details">
@@ -144,7 +158,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
 
         <!-- end tiket-selection -->
@@ -171,7 +185,8 @@
             <div class="datepicker align" id="datepicker"></div>
             <div class="content">
                 <p class="note">* Quý khách chú ý, ngày sử dụng không được thay đổi sau khi đã đặt vé, vé đã bán
-                    không hoàn trả lại. Mọi thắc mắc vui lòng liên hệ với chúng tôi theo <b style="color: yellow">hotline 0985 355 861 hoặc 1900 066 808 bấm phím 1</b></p>
+                    không hoàn trả lại. Mọi thắc mắc vui lòng liên hệ với chúng tôi theo <b
+                        style="color: yellow">hotline 0985 355 861 hoặc 1900 066 808 bấm phím 1</b></p>
             </div>
             <div class="buy-tickets__by-date">
                 <button class="btn-buy-tickets_by-date btn-select-date">Chọn ngày</button>
